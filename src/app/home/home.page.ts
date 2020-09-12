21a8caf5b8f482ac9259bb8fe5c3656c747c8e50
Your Name <you@example.com>
2020-09-12 12:24:04 -0300

    Integracion con google map y geolo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+declare var google;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+  
+export class HomePage implements OnInit{
+  public point = { lat: 0, long: 0 };
+  
 
-  constructor() {}
+  constructor(private geolocation:Geolocation) {}
 
+  public async ngOnInit() {
+    let geoloc = await this.geolocation.getCurrentPosition();  
+    this.point.lat = geoloc.coords.latitude;
+    this.point.long = geoloc.coords.longitude;
+    console.log(this.point)
+    this.generarMapa();
+  }
+  public generarMapa() {
+
+    const divMapa = document.getElementById("container");
+    const map = new google.maps.Map(divMapa,
+      {
+        center: {
+          lat: this.point.lat,
+          lng: this.point.long
+        },
+        zoom: 12
+      }
+    );
+    const punto = new google.maps.Marker({
+      position: {
+        lat: this.point.lat,
+        lng: this.point.long
+      },
+      title: "Estoy aca!!!",
+      map: map
+    });
+   
+  }
+  
 }
